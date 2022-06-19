9fdbf6001d35e18e49301f9ef47f16cbf38146fc
MicroProgramers <79642399+MicroProgramers@users.noreply.github.com>
2022-06-19 15:07:42 +0500

    Update firebase_configuration.js

--- js/firebase_configuration.js
+++ js/firebase_configuration.js
@@ -38,11 +38,14 @@ $(document).ready(function () {
                     })
                         .then(function (docRef) {
                             console.log("Document written with ID: ", docRef.id);
-                            $("#notifs-form-btn").text("Notified!");
+                            $("#notifs-form-btn").text("Subscribed!");
+                            alert('Updates enabled for this email');
                         })
                         .catch(function (error) {
                             console.error("Error adding document: ", error);
                         });
+                } else {
+                    alert('Already Subscribed!');
                 }
             });
 
